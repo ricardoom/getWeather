599b6dc4050c077fa439b642e20b68ce2e24cee0
Ricardo E. Galvez <galvez667@gmail.com>
2022-03-07 15:47:56 -0800

    :meat_on_bone: futzing w/ words and weather so I can proudly start using in my portfolio. good progress. mucked somewhat w/ the way type is being displyed - using css custom props. to chagne type size in a more interesting way.

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 //
 // Word getter and doer with things...
 
-import { wordnikAPIKey, openWeatherAPIKey } from "./keys.js";
+import { wordnikAPIKey, openWeatherAPIKey } from './keys.js';
 import { getRandom, getRandomInt, getRandomArrayElement } from './random.js';
 
+function createNode(el) {
+  return document.createElement(el);
+}
 
-  function createNode(el) {
-    return document.createElement(el);
-  }
-  
-  function append(parent, el) {
-    return parent.appendChild(el);
-  }
+function append(parent, el) {
+  return parent.appendChild(el);
+}
 
 // http://api.wordnik.com/v4/words.json/randomWord?api_key=YOURKEYHERE
 
 const wordnikURL = 'http://api.wordnik.com/v4/words.json/randomWords?api_key=';
 
-const wordnikURLLimited = 'https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=10&maxLength=-1&limit=15&api_key=';
+const wordnikURLLimited =
+  'https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=10&maxLength=-1&limit=15&api_key=';
 
 const fullWordnikURL = `${wordnikURLLimited}${wordnikAPIKey}`;
 
 const city_id = 5110302; // Brooklyn Ny, USA
-const weatherURL = `https://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${openWeatherAPIKey}`
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${openWeatherAPIKey}`;
 
 const body = document.querySelector('body');
 const svg = document.querySelector('svg');
@@ -43,9 +43,8 @@ const someValue = null;
 // convert the above to function that returns, the two values as an object
 
 const makeCoordinate = (x, y) => {
-  return { x, y }
-}
-
+  return { x, y };
+};
 
 // console.log(coords);
 
@@ -54,49 +53,59 @@ const makeCoordinate = (x, y) => {
 // no joke!
 
 fetch(fullWordnikURL)
-  .then(response => (response.json()))
+  .then((response) => response.json())
   // .then(data => (console.log(data)))
-  .then(data => {
+  .then((data) => {
     let words = data;
-    
-    words.forEach(word => {
+
+    words.forEach((word) => {
       // let svgText = createNode('text');
-      let svgText = document.createElementNS('http://www.w3.org/2000/svg','text');
-      let newY = (getRandomInt(vw));
-      let newX = (getRandomInt(vh));
+      let svgText = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+      let newY = getRandomInt(vw);
+      let newX = getRandomInt(vh);
       let coords = makeCoordinate(newX, newY);
       const h = getRandomInt(360);
       const s = getRandomInt(100);
       const l = getRandomInt(100);
       const a = getRandom(1, 0.27);
-      const rotations = [
-        0, 90, 180, 270, 45, 225, 315
-      ];
+      const rotations = [0, 90, 180, 270, 45, 225, 315];
       const rotated = getRandomArrayElement(rotations);
+      let fs = getRandomInt(5);
       svgText.innerHTML = `${word.word}`;
       svgText.setAttribute('transform', `translate(${coords.x * 0.5} ${coords.y * 0.5}) rotate(${rotated})`);
       svgText.setAttribute('x', coords.x * 0.01);
       svgText.setAttribute('y', coords.y * 0.01); // fix the -y viewBox Viewport thing
       svgText.setAttribute('style', `fill: hsla(${h}, ${s}%, ${l}%, ${a.toFixed(2)})`);
       svgText.setAttribute('rotate', `${rotated}`);
-      
+      svgText.setAttribute('font-size', `calc(10 * var(--s${fs}))`);
+
       // console.log(svgText.innerHTML);
-      append(svg, svgText)
-    })
+      append(svg, svgText);
+    });
   })
-  .catch(err => (console.error(err)));
+  .catch((err) => console.error(err));
 
-  // get the weather
+// get the weather
 fetch(weatherURL)
-.then(response => (response.json()))
-.then(data => {
-  const styleBG = body.style;
-  const weather = data;
-  const { deg, speed } = weather.wind;
-  const { temp } = weather.main;
-  const useSpeed = Math.floor(speed) * 10;
-  console.log(weather, deg, speed, temp, styleBG, useSpeed);
+  .then((response) => response.json())
+  .then((data) => {
+    const styleBG = body.style;
+    const weather = data;
+    const { deg, speed } = weather.wind;
+    const { temp } = weather.main;
+    const useSpeed = Math.floor(speed) * 10;
+    // console.log(weather, deg, speed, temp, styleBG, useSpeed);
 
-  body.setAttribute(`style`, `background-image: radial-gradient(hsl(${deg}, 72%, 45%), hsl(${temp}, ${useSpeed}%, ${useSpeed * 1.2}%));`)
-})
-.catch(console.error);
+    body.setAttribute(`style`, `background-image: radial-gradient(hsl(${deg}, 72%, 45%), hsl(${temp}, ${useSpeed}%, ${useSpeed * 1.2}%));`);
+  })
+  .catch(console.error);
+
+// some attributes to pass in to the attribs arg, should be an array:
+
+const useWeather = (element, ...attribs) => {
+  element.setAttribute(`style`, `fontSize: ${attribs[0]}`);
+  console.log(element, attribs[0]);
+};
+const incomingWeather = [];
+const weatherUno = useWeather(body, incomingWeather);
+weatherUno;
